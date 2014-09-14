2df2c532f65e21c8e8c2bea0182e7f214b5f0644
Carlos Agüero <caguero@osrfoundation.org>
2014-09-14 17:40:46 +0000

    README.md edited online with Bitbucket

---
+++ README.md
@@ -0,0 +1,67 @@
+# Ignition transport
+
+** Ignition transport classes and functions for robot applications.**
+
+Ignition transport is a component in the ignition framework, a set
+of libraries designed to rapidly develop robot applications.
+
+  [http://ignitionrobotics.org](http://ignitionrobotics.org)
+
+## Continuous integration
+
+Please refer to the [drone.io
+job](https://drone.io/bitbucket.org/ignitionrobotics/ign-transport).
+
+[![Build Status](https://drone.io/bitbucket.org/ignitionrobotics/ign-transport/status.png)](https://drone.io/bitbucket.org/ignitionrobotics/ign-transport/latest)
+
+
+## Dependencies
+
+The following dependencies are required to compile ignition-transport from
+source:
+
+ - uuid-dev
+ - libzmq3-dev
+ - czmq
+ - ignition-msgs
+ - cmake
+ - ruby-ronn
+ - mercurial
+ - C++ compiler with c++11 support (eg. GCC>=4.8).
+
+**Note:** *if you are using an Ubuntu platform previous to Saucy, you will need to install zeromq from source, since there is no libzmq3-dev*
+
+    sudo apt-get install build-essential uuid-dev libprotobuf-dev protobuf-compiler libprotoc-dev libzmq3-dev cmake ruby-ronn git mercurial libtool automake
+
+    git clone git://github.com/zeromq/czmq.git
+    cd czmq
+    ./autogen.sh
+    ./configure && make check
+    sudo make install
+    sudo ldconfig
+    cd ..
+
+    hg clone https://bitbucket.org/ignitionrobotics/ign_msgs
+    cd ign_msgs
+    mkdir build
+    cd build
+    cmake .. -DCMAKE_INSTALL_PREFIX=/usr
+    sudo make install
+    cd ..
+
+## Installation
+
+Standard installation can be performed in UNIX systems using the following
+steps:
+
+ - mkdir build/
+ - cd build/
+ - cmake .. -DCMAKE_INSTALL_PREFIX=/usr
+ - sudo make install
+
+## Uninstallation
+
+To uninstall the software installed with the previous steps:
+
+ - cd build/
+ - sudo make uninstall
---
+++ test/integration/twoProcessesPubSub.cc
@@ -0,0 +1,121 @@
+/*
+ * Copyright (C) 2014 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+#include <string>
+#include <chrono>
+#include <ignition/msgs.hh>
+#include "ignition/transport/Node.hh"
+#include "gtest/gtest.h"
+
+using namespace ignition;
+
+bool cbExecuted;
+bool cb2Executed;
+
+std::string topic = "/foo";
+std::string data = "bar";
+
+
+//////////////////////////////////////////////////
+/// \brief Function is called everytime a topic update is received.
+void cb(const std::string &_topic, const ignition::msgs::Vector3d &_msg)
+{
+  EXPECT_EQ(_topic, topic);
+  EXPECT_FLOAT_EQ(_msg.x(), 1.0);
+  EXPECT_FLOAT_EQ(_msg.y(), 2.0);
+  EXPECT_FLOAT_EQ(_msg.z(), 3.0);
+  cbExecuted = true;
+}
+
+//////////////////////////////////////////////////
+/// \brief Function is called everytime a topic update is received.
+void cb2(const std::string &_topic, const ignition::msgs::Vector3d &_msg)
+{
+  EXPECT_EQ(_topic, topic);
+  EXPECT_FLOAT_EQ(_msg.x(), 1.0);
+  EXPECT_FLOAT_EQ(_msg.y(), 2.0);
+  EXPECT_FLOAT_EQ(_msg.z(), 3.0);
+  cb2Executed = true;
+}
+
+//////////////////////////////////////////////////
+void runSubscriber()
+{
+  cbExecuted = false;
+  cb2Executed = false;
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  transport::Node node;
+  transport::Node node2;
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  EXPECT_TRUE(node.Subscribe(topic, cb));
+  EXPECT_TRUE(node2.Subscribe(topic, cb2));
+  std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+  // Check that the message was received.
+  EXPECT_TRUE(cbExecuted);
+  EXPECT_TRUE(cb2Executed);
+  cbExecuted = false;
+  cb2Executed = false;
+
+  EXPECT_TRUE(node.Unsubscribe(topic));
+  std::this_thread::sleep_for(std::chrono::milliseconds(600));
+
+  // Check that the message was only received in node3.
+  EXPECT_FALSE(cbExecuted);
+  EXPECT_TRUE(cb2Executed);
+  cbExecuted = false;
+  cb2Executed = false;
+}
+
+//////////////////////////////////////////////////
+/// \brief Three different nodes running in two different processes. In the
+/// subscriber processs there are two nodes. Both should receive the message.
+/// After some time one of them unsubscribe. After that check that only one
+/// node receives the message.
+TEST(twoProcPubSub, PubSubTwoProcsTwoNodes)
+{
+  pid_t pid = fork();
+
+  if (pid == 0)
+    runSubscriber();
+  else
+  {
+    ignition::msgs::Vector3d msg;
+    msg.set_x(1.0);
+    msg.set_y(2.0);
+    msg.set_z(3.0);
+
+    transport::Node node1;
+
+    EXPECT_TRUE(node1.Advertise(topic));
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    EXPECT_TRUE(node1.Publish(topic, msg));
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    EXPECT_TRUE(node1.Publish(topic, msg));
+
+    // Wait for the child process to return.
+    int status;
+    waitpid(pid, &status, 0);
+  }
+}
+
+//////////////////////////////////////////////////
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
