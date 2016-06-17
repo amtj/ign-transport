4b544a0228c7cfa06613b9135ba0326f71544f13
Amitoj <iamamitoj@gmail.com.com>
2016-06-18 00:58:09 +0530

    Add example/SrvProviderOneway

---
+++ example/SrvProviderOneway.cc
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2014 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include <iostream>
+#include <string>
+#include <ignition/transport.hh>
+#include "msgs/stringmsg.pb.h"
+
+//////////////////////////////////////////////////
+int main(int argc, char **argv)
+{
+  // Create a transport node.
+  ignition::transport::Node node;
+
+  // Prepare the input parameters.
+  std::string topic = "/foo";
+
+  // Advertise a topic.
+  bool executed = node.Advertise<example::msgs::StringMsg>(topic);
+
+  if (executed)
+  {
+    std::cout << "Advertise succeeded" << std::endl;
+  }
+  else
+    std::cerr << "Advertise failed" << std::endl;
+}
